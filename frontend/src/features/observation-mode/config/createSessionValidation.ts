5c9cc992e128e0dea94d1a203acacdcf1c8c7fd3
yrajapa <yrajapa@ncsu.edu>
2026-04-09 20:36:57 -0400

    Add session validation

---
+++ frontend/src/features/observation-mode/config/createSessionValidation.ts
@@ -0,0 +1,6 @@
+export const JOIN_CODE_ALREADY_EXISTS_MESSAGE =
+  "Join code already exists. Generate a new code or enter a different custom code.";
+
+export const JOIN_CODE_LOOKUP_ERROR_MESSAGE =
+  "Unable to verify that join code right now. Please try again.";
+
